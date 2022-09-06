82549e9f3b50800ade16200488b2f0de183e7a30
Kroquetitaa <Kroquetita@gmail.com>
2022-09-07 01:56:52 +0200

    readjusting schema items and quit __v/_id

--- src/infraestructure/schemas/items.schema.js
+++ src/infraestructure/schemas/items.schema.js
@@ -3,24 +3,33 @@ const { v4: uuidv4 } = require('uuid');
 const DATAVALUE = require('../../utils/constants/DATAVALUE.js');
 const { Schema } = mongoose;
 
-const ItemSchema = new Schema({
-  _id: {
-    type: String,
-    default: function genUUID() {
-      return uuidv4();
+const ItemSchema = new Schema(
+  {
+    _id: {
+      type: String,
+      default: function genUUID() {
+        return uuidv4();
+      },
     },
+    item: Number,
+    minecraftIDName: { type: String, unique: true },
+    minecraftID: { type: Number, unique: true },
+    minecraftDataValue: { type: Number, enum: DATAVALUE },
+    flamableItem: Boolean,
+    rarityColor: { rarityColorSpanish: String, rarityColorEnglish: String },
+    imageItem: String,
+    craftingImage: String,
+    nameItem: { nameItemSpanish: String, nameItemEnglish: String },
   },
-  item: Number,
-  minecraftIDName: {type: String, unique: true},
-  minecraftID: { type: Number, unique: true },
-  minecraftDataValue: { type: Number, enum: DATAVALUE },
-  flamableItem: Boolean,
-  rarityColor: [
-    { type: Schema.Types.Mixed, ref: 'rarityColor', required: false },
-  ],
-  imageItem: String,
-  craftingImage: String,
-  nameItem: [{ type: Schema.Types.Mixed, ref: 'nameItem', required: false }],
-});
+  //   {
+  //     toJSON: {
+  //       virtuals: false,
+  //       versionKey: false,
+  //       transform: function (doc, ret) {
+  //         delete ret._id;
+  //       },
+  //     },
+  //   },
+);
 
 module.exports = mongoose.model('items', ItemSchema);
--- src/infraestructure/schemas/versions.schema.js
+++ src/infraestructure/schemas/versions.schema.js
@@ -2,16 +2,27 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 const { v4: uuidv4 } = require('uuid');
 
-const VersionSchema = new Schema({
-  _id: {
-    type: String,
-    default: function genUUID() {
-      return uuidv4();
+const VersionSchema = new Schema(
+  {
+    _id: {
+      type: String,
+      default: function genUUID() {
+        return uuidv4();
+      },
+    },
+    version: String,
+    items: [{ type: Schema.Types.Mixed, ref: 'items' }],
+  },
+  {
+    toJSON: {
+      virtuals: false,
+      versionKey: false,
+      transform: function (doc, ret) {
+        delete ret._id;
+      },
     },
   },
-  version: String,
-  items: [{ type: Schema.Types.Mixed, ref: 'items' }],
-});
+);
 
 function autopopulate(next) {
   this.populate({ path: 'items' });
