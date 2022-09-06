d2a8cc0703d855cc4f06935edacdbe215c8582be
Kroquetitaa <Kroquetita@gmail.com>
2022-09-06 23:39:27 +0200

    add uuid for _id

--- src/infraestructure/schemas/versions.schema.js
+++ src/infraestructure/schemas/versions.schema.js
@@ -1,9 +1,23 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
+const { v4: uuidv4 } = require('uuid');
 
 const VersionSchema = new Schema({
+  _id: {
+    type: String,
+    default: function genUUID() {
+      return uuidv4();
+    },
+  },
   version: String,
-  items: [{ type: Schema.ObjectId, ref: 'items' }],
+  items: [{ type: Schema.Types.Mixed, ref: 'items' }],
 });
+
+function autopopulate(next) {
+  this.populate({ path: 'items' });
+  next();
+}
+
+VersionSchema.pre('find', autopopulate);
 
 module.exports = mongoose.model('versions', VersionSchema);
