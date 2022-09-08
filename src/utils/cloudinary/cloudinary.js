ea64af6e7b5ecde2e565df092372020cf02abf4d
Kroquetitaa <Kroquetita@gmail.com>
2022-09-08 13:41:22 +0200

    add cloudinary and public_id for images

---
+++ src/utils/cloudinary/cloudinary.js
@@ -0,0 +1,11 @@
+const cloudinary = require("cloudinary");
+
+const setUpCloudinary = () => {
+  cloudinary.config({
+    cloud_name: process.env.CLOUD_NAME,
+    api_key: process.env.API_KEY,
+    api_secret: process.env.API_SECRET,
+  });
+}
+
+module.exports = { setUpCloudinary }
