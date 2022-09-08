e11a3b079c17208f1e2557e14d4c1b12206be818
Kroquetitaa <Kroquetita@gmail.com>
2022-09-08 11:33:20 +0200

    up testing of testItems and testGames

--- test/testGames.js
+++ test/testGames.js
@@ -2,14 +2,22 @@ const chai = require('chai');
 const chaiHttp = require('chai-http');
 const expect = require('chai').expect;
 chai.use(chaiHttp);
-const url = 'http://localhost:8080/api/versions';
+const url = 'http://localhost:8080/api/games';
 
 describe('Testing 1: create a Version', () => {
   it('Should create a new version', (done) => {
     chai
       .request(url)
       .post('/create')
-      .send({ version: 1 })
+      .send({
+        id: 5,
+        game: 'prueba',
+        price: 1,
+        releaseData: 'prueba',
+        image: 'prueba',
+        titles: ['prueba'],
+        description: 'prueba',
+      })
       .end((err, res) => {
         console.log(res.body);
         expect(res).to.have.status(200);
@@ -30,7 +38,3 @@ describe('Testing 2: Get all Versions', () => {
       });
   });
 });
-
-
-
-
--- test/testItems.js
+++ test/testItems.js
@@ -2,14 +2,20 @@ const chai = require('chai');
 const chaiHttp = require('chai-http');
 const expect = require('chai').expect;
 chai.use(chaiHttp);
-const url = 'http://localhost:8080/api/versions';
+const url = 'http://localhost:8080/api/items';
 
 describe('Testing 1: create a Version', () => {
   it('Should create a new version', (done) => {
     chai
       .request(url)
       .post('/create')
-      .send({ version: 1 })
+      .send({
+        id: 5,
+        minecraftIDName: 'prueba',
+        minecraftID: 1,
+        minecraftDataValue: 0,
+        flamableItem: false,
+      })
       .end((err, res) => {
         console.log(res.body);
         expect(res).to.have.status(200);
@@ -30,7 +36,3 @@ describe('Testing 2: Get all Versions', () => {
       });
   });
 });
-
-
-
-
